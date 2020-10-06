a6e13c0a7c7ca1b301055fb4e1120fa3bfd6e370
tanyaapb <72110047+tanyaapb@users.noreply.github.com>
2020-10-05 23:47:26 -0700

    Dict changes

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -4,15 +4,28 @@
 from melons import melon_names, melon_seedlessness, melon_prices
 
 
-def print_melon(name, seedless, price):
+# def print_melon(name, seedless, price):
+def print_melon():
     """Print each melon with corresponding attribute information."""
+ # create a big dictinary containing dictionary and a set for melong and attributes
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    merged_dict = {}
+    for key,value in melon_names.items():
+        price = melon_prices.get(key, 0)
+        seedless = melon_seedlessness.get(key, False)
+        merged_dict[value.upper()] = [price, seedless]
+    
+    for key, value in merged_dict.items():
+        name = key
+        price = value[0]
+        seedless = value[1]
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+        have_or_have_not = 'have'
+        if seedless:
+            have_or_have_not = 'do not have'
 
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+        print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+        # print(f"{key}: {value}")
+    
+    
+print_melon()
